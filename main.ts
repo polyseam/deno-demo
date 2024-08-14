7a90baf698e3b69460d41ed4b79c0aa1aa84bb07
Matt Johnston <m@ttjohnston.com>
2024-08-14 19:07:47 -0300

    example

---
+++ main.ts
@@ -0,0 +1,23 @@
+const whoToGreet = Deno.env.get("WHO_TO_GREET") || "World";
+
+Deno.serve((req: Request): Response => {
+  const { url } = req;
+  const message = "hello";
+
+  const body: string = JSON.stringify({
+    message: `${message} ${whoToGreet}`,
+    metadata: {
+      url,
+    },
+  });
+
+  return new Response(
+    body,
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    },
+  );
+});
